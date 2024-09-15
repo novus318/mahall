1c99522db18a0b5bc2b3968a24f8930fb91b10c1
novus318 <nizamudheen318@gmail.com>
2024-09-15 18:37:06 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,67 +40,69 @@ function Home() {
       {/* Main Section */}
       <main className="">
       <section className="relative bg-white">
-      <div className="relative overflow-hidden md:pb-24">
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={sectionVariants}
-          className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 flex flex-col lg:flex-row items-center"
+  <div className="relative overflow-hidden md:pb-20">
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={sectionVariants}
+      className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-24 flex flex-col lg:flex-row items-center"
+    >
+      <div className="text-center lg:text-left lg:w-1/2">
+        <motion.h1
+          className="text-4xl font-bold sm:text-5xl md:text-6xl"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0, transition: { duration: 0.8, ease: 'easeOut' } }}
         >
-          {/* Text Content */}
-          <div className="text-center lg:text-left lg:w-1/2">
-            <motion.h1
-              className="text-4xl font-bold sm:text-5xl md:text-6xl"
-              initial={{ opacity: 0, x: -50 }}
-              animate={{ opacity: 1, x: 0, transition: { duration: 0.8, ease: 'easeOut' } }}
-            >
-              Vellap Khadimul Islam Jamaath
-            </motion.h1>
-            <p className="mt-3 max-w-md mx-auto text-lg md:mt-5 md:text-xl md:max-w-3xl font-medium">
-            لجنة جماعة خادم الإسلام
-              </p><p className="mt-3 max-w-md mx-auto text-lg md:mt-5 md:text-xl md:max-w-3xl font-medium">Purity Comes From Faith, Belief in the Unity of Allah & Prophet Muhammad <span className='text-4xl mx-2'>ﷺ</span> as Allah’s messenger.
-            </p>
-          </div>
-          {/* Mosque Image */}
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            variants={imageVariants}
-            className="mt-8 lg:mt-0 lg:w-1/2 flex justify-center lg:justify-end"
-          >
-            <Image
-              src="/mosque.JPG"
-              alt="VKJ"
-              width={600}
-              height={400}
-              className="rounded-lg shadow-xl"
-            />
-          </motion.div>
-        </motion.div>
-
-        <div className="absolute bottom-0 left-0 w-full">
-          <svg
-            viewBox="0 0 1440 320"
-            xmlns="http://www.w3.org/2000/svg"
-            className="fill-current text-gray-900"
-          >
-            {/* Waves with Shadows */}
-            <path
-              fill="#032e2c"
-              fillOpacity="1"
-              d="M0,64L60,74.7C120,85,240,107,360,128C480,149,600,171,720,165.3C840,160,960,128,1080,106.7C1200,85,1320,75,1380,69.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
-              style={{ filter: 'drop-shadow(0px -10px 15px rgba(0, 0, 0, 0.5))' }}
-            />
-            <path
-              fill="#011615"
-              fillOpacity="1"
-              d="M0,192L60,186.7C120,181,240,171,360,160C480,149,600,139,720,128C840,117,960,107,1080,117.3C1200,128,1320,160,1380,160L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
-              style={{ filter: 'drop-shadow(0px -10px 20px rgba(0, 0, 0, 0.5))' }}
-            />
-          </svg>
-        </div>
+          Vellap Khadimul Islam Jamaath
+        </motion.h1>
+        <p className="mt-3 max-w-md mx-auto text-lg md:mt-5 md:text-xl md:max-w-3xl font-medium">
+          لجنة جماعة خادم الإسلام
+        </p>
+        <p className="mt-3 max-w-md mx-auto text-lg md:mt-5 md:text-xl md:max-w-3xl font-medium">
+          Purity Comes From Faith, Belief in the Unity of Allah & Prophet Muhammad <span className="text-4xl mx-2">ﷺ</span> as Allah’s messenger.
+        </p>
       </div>
-    </section>
+
+      {/* Mosque Image */}
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={imageVariants}
+        className="mt-8 lg:mt-0 lg:w-1/2 flex justify-center lg:justify-end relative z-0"
+      >
+        <Image
+          src="/mosque.JPG"
+          alt="VKJ"
+          width={600}
+          height={400}
+          className="rounded-lg shadow-xl"
+        />
+      </motion.div>
+    </motion.div>
+
+    <div className="absolute bottom-14 md:bottom-0 left-0 w-full z-20"> 
+      <svg
+        viewBox="0 0 1440 320"
+        xmlns="http://www.w3.org/2000/svg"
+        className="fill-current text-gray-900"
+      >
+        <path
+          fill="#032e2c"
+          fillOpacity="1"
+          d="M0,64L60,74.7C120,85,240,107,360,128C480,149,600,171,720,165.3C840,160,960,128,1080,106.7C1200,85,1320,75,1380,69.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          style={{ filter: 'drop-shadow(0px -10px 15px rgba(0, 0, 0, 0.5))' }}
+        />
+        <path
+          fill="#011615"
+          fillOpacity="1"
+          d="M0,192L60,186.7C120,181,240,171,360,160C480,149,600,139,720,128C840,117,960,107,1080,117.3C1200,128,1320,160,1380,160L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          style={{ filter: 'drop-shadow(0px -10px 20px rgba(0, 0, 0, 0.5))' }}
+        />
+      </svg>
+    </div>
+  </div>
+</section>
+
 
 
        <PrayerTimes/>
